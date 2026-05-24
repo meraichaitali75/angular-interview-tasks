82de5a94604f3c80dc8958b88e87c94df41b6db9
meraichaitali75 <meraichaitali75@gmail.com>
2026-05-24 16:44:15 -0400

    fix: api data displayed using observable and signal.

--- src/app/api/fetchapi.component.ts
+++ src/app/api/fetchapi.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { catchError, Observable, of } from 'rxjs';
 export interface User {
   id: number;
@@ -11,7 +12,7 @@ export interface User {
   selector: 'fetchapi-component',
   imports: [CommonModule],
   template: `
-      <h1>FETCH API DATA</h1>
+      <h1>RXjs Observable Users</h1>
       @if(users$ | async; as usersList){
         @if(usersList.length > 0){
         <table>
@@ -26,6 +27,20 @@ export interface User {
         </table>
         }
       }
+    <h1>Writable Signal + Interop Bridge</h1>
+      @if(usersSignalList().length > 0){
+      <table>
+        <tbody>
+        @for(user of usersSignalList(); track user.id){
+        <tr>
+          <td>{{user.id}}</td>
+          <td>{{user.name}}</td>
+        </tr>
+        }
+        </tbody>
+      </table>
+      }
+  
   `,
 })
 export class FetchApiComponent implements OnInit {
@@ -33,12 +48,19 @@ export class FetchApiComponent implements OnInit {
   private readonly apiUrl = "https://jsonplaceholder.typicode.com/users";
   private http = inject(HttpClient);
 
+  //RXjs observable
   users$: Observable<User[]> = of([]);
-
   ngOnInit(): void {
-    this.users$ = this.getUsers();
+    this.users$ = this.fetchUsersFromApi();
   }
-  private getUsers(): Observable<User[]> {
+
+  //Signal Strem data
+  public usersSignalList = toSignal(
+    this.fetchUsersFromApi(),
+    { initialValue: [] } // Safe fallback synchronous layout collection array
+  );
+
+  private fetchUsersFromApi(): Observable<User[]> {
     return this.http.get<User[]>(this.apiUrl).pipe(
       catchError((error) => {
         return of([]);
